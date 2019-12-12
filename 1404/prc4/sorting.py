1e777bb74a1795bf1d02c15251ddfe90f8048faf
Jihui Wen <jihui.wen@my.jcu.edu.au>
2019-12-12 18:06:41 +0800

    final

--- 1404/prc4/sorting.py
+++ 1404/prc4/sorting.py
@@ -15,8 +15,8 @@ data = [['Derek', 7], ['Carrie', 7], ['Bob', 6], ['Aaron', 6]]
 data.sort(key=itemgetter(1, 0))
 print(data)
 
-#TODO: sort the following list of tuples by last name then first name
+# TODO: sort the following list of tuples by last name then first name
 items = [('123', 'Derek', 'Smith', 7), ('124', 'Carrie', 'Brown', 7),
          ('125', 'Bob', 'Smith', 6), ('126', 'Aaron', 'Hewitt', 6)]
-
+# ??
 print(items)
